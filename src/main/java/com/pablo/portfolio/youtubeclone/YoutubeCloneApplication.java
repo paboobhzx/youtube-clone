310052876deba4deb38f84c2371d9df8efcb4776
Pablo <pablobhz@gmail.com>
2023-03-27 15:36:12 -0300

    Innitial Commit

---
+++ src/main/java/com/pablo/portfolio/youtubeclone/YoutubeCloneApplication.java
@@ -0,0 +1,13 @@
+package com.pablo.portfolio.youtubeclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeCloneApplication.class, args);
+	}
+
+}
